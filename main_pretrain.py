a746b717c0bd705d0e3264bb5e890bc03067caeb
Nayeon <kny5265@naver.com>
2024-05-24 22:46:20 +0900

    Update main_pretrain.py

--- main_pretrain.py
+++ main_pretrain.py
@@ -62,35 +62,34 @@ def main(args):
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
     log_writer = None
-    # for epoch in range(args.start_epoch, args.epochs):
-    #     train_stats = train_one_epoch(
-    #         model, dataloader['train'],
-    #         optimizer, epoch, loss_scaler,
-    #         log_writer=log_writer,
-    #         args=args, iter=epoch
-    #     )
-    #     if args.output_dir and (epoch % args.save_freq == 0 or epoch + 1 == args.epochs):
-    #         misc.save_model(
-    #             args=args, model=model, model_without_ddp=model, optimizer=optimizer,
-    #             loss_scaler=loss_scaler, epoch=epoch)
+    for epoch in range(args.start_epoch, args.epochs):
+        train_stats = train_one_epoch(
+            model, dataloader['train'],
+            optimizer, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args, iter=epoch
+        )
+        if args.output_dir and (epoch % args.save_freq == 0 or epoch + 1 == args.epochs):
+            misc.save_model(
+                args=args, model=model, model_without_ddp=model, optimizer=optimizer,
+                loss_scaler=loss_scaler, epoch=epoch)
 
-    #     log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
-    #                     'epoch': epoch,}
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                        'epoch': epoch,}
 
-    #     if args.output_dir and misc.is_main_process():
-    #         if log_writer is not None:
-    #             log_writer.flush()
-    #         with open(os.path.join(args.savedir, "log.txt"), mode="a", encoding="utf-8") as f:
-    #             f.write(json.dumps(log_stats) + "\n")
+        if args.output_dir and misc.is_main_process():
+            if log_writer is not None:
+                log_writer.flush()
+            with open(os.path.join(args.savedir, "log.txt"), mode="a", encoding="utf-8") as f:
+                f.write(json.dumps(log_stats) + "\n")
 
-    # total_time = time.time() - start_time
-    # total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    # print('Training time {}'.format(total_time_str))
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
 
-    # pattern = re.compile(r'\d+')
-    # file_list = sorted(glob.glob('{}/*.pth'.format(args.savedir)), key=lambda x:int(pattern.findall(x)[-1]))
-    # args.resume = file_list[-1]
-    args.resume = 'F:\kny\workspace\dts\diffmae-pytorch\output_dir\cifar10-20240523-2308-diffmae\checkpoint-99.pth'
+    pattern = re.compile(r'\d+')
+    file_list = sorted(glob.glob('{}/*.pth'.format(args.savedir)), key=lambda x:int(pattern.findall(x)[-1]))
+    args.resume = file_list[-1]
     misc.load_model(args=args, model_without_ddp=model, optimizer=optimizer, loss_scaler=loss_scaler)
 
     if args.eval:
